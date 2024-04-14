ea27f6c590f3e2579fa18a542cf9597c9013b7af
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-04-14 17:38:49 -0400

    feat: add endpoint to see post (#4)

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -97,3 +97,36 @@ export const updatePost = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getPost = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error to get product',
+      error,
+    });
+  }
+};
+
+export const getAllPost = async (req: Request, res: Response) => {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        userId: getTokenId(req),
+      },
+    });
+    return res.status(200).json(products);
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error to get products',
+      error,
+    });
+  }
+};
--- backend/src/router/product.router.ts
+++ backend/src/router/product.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { createPost, deletePost, updatePost } from '../controllers/product.controller';
+import { createPost, deletePost, getAllPost, getPost, updatePost } from '../controllers/product.controller';
 
 const router = Router();
 
 router.post('/create', createPost);
 router.delete('/delete/:id', deletePost);
 router.put('/update/:id', updatePost);
+router.get('/:id', getPost);
+router.get('/', getAllPost);
 
 export default router;
