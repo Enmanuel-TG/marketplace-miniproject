6046c13f6d3adfab0b2fdd5c581f32ece72728a6
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-04-15 02:40:32 -0400

    feat: implement validators to createPost (#4)

--- backend/src/router/product.router.ts
+++ backend/src/router/product.router.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { createPost, deletePost, getAllPost, getPost, updatePost } from '../controllers/product.controller';
+import postValidator from '../validators/post.validators.ts';
+import validateSchema from '../middlewares/validator.middleware.ts';
 
 const router = Router();
 
-router.post('/create', createPost);
+router.post('/create', validateSchema(postValidator), createPost);
 router.delete('/delete/:id', deletePost);
-router.put('/update/:id', updatePost);
+router.put('/update/:id', validateSchema(postValidator), updatePost);
 router.get('/:id', getPost);
 router.get('/', getAllPost);
 
