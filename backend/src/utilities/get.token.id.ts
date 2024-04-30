e66635628aecb2e121f1f54b56a10cef4b342ec3
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-04-29 22:34:48 -0400

    feat: add photo when creating the product

--- backend/src/utilities/get.token.id.ts
+++ backend/src/utilities/get.token.id.ts
@@ -5,6 +5,7 @@ import { ExtendedRequest } from '../types.d';
 export const getTokenId = (req: ExtendedRequest) => {
   const token = req.cookies.token;
   const decoded = jwt.verify(token, TOKEN_SECRET) as unknown as { id: string; iat: number; exp: number };
+
   req.userId = Number(decoded.id);
   const id = Number(decoded.id);
   return id;
