b6e990a709c0a93c2237dd94f42e085798a00070
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-06-10 19:22:20 -0400

    feat: add endpoint  to get product by category

--- backend/src/router/product.router.ts
+++ backend/src/router/product.router.ts
@@ -2,21 +2,23 @@ import { Router } from 'express';
 import {
   createProduct,
   deleteProduct,
+  getAllProduct,
   getAllUserProduct,
   getProduct,
+  getProductsByCategory,
   updateProduct,
-  getAllProduct,
 } from '../controllers/product.controller';
-import ProductValidator from '../validators/product.validator.ts';
-import validateSchema from '../middlewares/validator.middleware.ts';
+import validateSchema from '../middlewares/validator.middleware';
+import ProductValidator from '../validators/product.validator';
 
 const router = Router();
 
 router.post('/create', validateSchema(ProductValidator), createProduct);
 router.delete('/delete/:id', deleteProduct);
 router.put('/update/:id', validateSchema(ProductValidator), updateProduct);
-router.get('/:id', getProduct);
-router.get('/:id', getAllUserProduct);
-router.get('/', getAllProduct);
+router.get('/product/:id', getProduct);
+router.get('/userProduct/:id', getAllUserProduct);
+router.get('/allProduct', getAllProduct);
+router.get('/category', getProductsByCategory);
 
 export default router;
