cf8389318a9da63bd4376f0d8e91272dc06a4282
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-06-17 23:57:00 -0400

    feat: auth eith  google in login page

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,32 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
-function AuthPage() {
-  const [user, setUser] = useState([]);
-  const [profile, setProfile] = useState([]);
+function App() {
+  const [ user, setUser ] = useState([]);
+  const [ profile, setProfile ] = useState([]);
 
   const login = useGoogleLogin({
-    onSuccess: (codeResponse) => setUser(codeResponse),
+    onSuccess: (codeResponse) => {setUser(codeResponse), console.log(codeResponse);},
     onError: (error) => console.log('Login Failed:', error),
   });
 
-  useEffect(() => {
-    if (user) {
-      axios
-        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-          headers: {
-            Authorization: `Bearer ${user.access_token}`,
-            Accept: 'application/json',
-          },
-        })
-        .then((res) => {
-          setProfile(res.data);
-        })
-        .catch((err) => console.log(err));
-    }
-    console.log({ user, profile });
-  }, [user]);
+  useEffect(
+    () => {
+      if (user) {
+        console.log(user);
+        axios
+          .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json',
+            },
+          })
+          .then((res) => {
+            setProfile(res.data);
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    [ user ],
+  );
 
   // log out function to log the user out of google and set the profile array to null
   const logOut = () => {
@@ -50,9 +53,9 @@ function AuthPage() {
           <button onClick={logOut}>Log out</button>
         </div>
       ) : (
-        <button onClick={login}>Sign in with Google 🚀 </button>
+        <button onClick={() => login}>Sign in with Google 🚀 </button>
       )}
     </div>
   );
 }
-export default AuthPage;
+export default App;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,22 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import Input from '../components/ui/Input';
 import Button from '../components/ui/Button';
 import { useForm } from 'react-hook-form';
 import { Account } from '../utilities/interfaces.utility';
+import { useGoogleLogin, TokenResponse } from '@react-oauth/google';
+import axios from 'axios';
 
 const LoginPages = () => {
   const navigate = useNavigate();
   const { signIn, isAuthenticated, errors: errorLogin } = useAuth();
   const { handleSubmit, register } = useForm<Account>();
+  const [user, setUser] = useState<TokenResponse | null>(null);
+  const [ profile, setProfile ] = useState([]);
+  console.log(user, profile);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {setUser(codeResponse), console.log(codeResponse);},
+    onError: (error) => console.log('Login Failed:', error),
+  });
 
   useEffect(() => {
     if (isAuthenticated) {
       navigate('/');
     }
   }, [isAuthenticated]);
-
+  useEffect(
+    () => {
+      if (user) {
+        console.log(user);
+        axios
+          .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json',
+            },
+          })
+          .then((res) => {
+            setProfile(res.data);
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    [ user ],
+  );
   const setData = (data: Account) => {
     signIn(data);
   };
@@ -44,9 +72,10 @@ const LoginPages = () => {
                   Register
                 </button>
               </div>
-              <Button fieldname="Login" type="submit"/>
+              <Button fieldname="Login" type="submit" />
             </div>
           </form>
+          <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => login}>Sign in with Google 🚀 </button>
         </div>
       </div>
     </div>
