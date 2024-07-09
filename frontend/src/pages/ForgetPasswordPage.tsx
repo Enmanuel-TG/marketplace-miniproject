87f8585a9f2fbbbe94160837b307e9eb25fd0f53
Enmanuel <62859680+Enmanuel-TG@users.noreply.github.com>
2024-07-09 17:43:09 +0000

    Enhance password reset page with styled and text

--- frontend/src/pages/ForgetPasswordPage.tsx
+++ frontend/src/pages/ForgetPasswordPage.tsx
@@ -13,10 +13,24 @@ export const ForgetPasswordPage = () => {
   const { handleSubmit, register } = useForm();
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-gray-100">
-      <form onSubmit={handleSubmit(setData)} className="w-full max-w-md mx-auto">
-        <Input fieldname="Email" type="email" {...register('email', { required: true })} />
-        <Button fieldname="Send" type="submit" />
-      </form>
+      <div className="w-full max-w-lg mx-auto bg-white shadow-md rounded p-8">
+        <h2 className="text-4xl text-center font-bold mb-6">Password Reset</h2>
+        
+        <p className="text-gray-700 text-base mb-4">
+          Please enter the email address associated with your account to reset your password.
+        </p>
+        <p className="text-gray-700 text-base mb-4">
+           You will receive an email with a link to reset your password.
+        </p>
+
+        <form onSubmit={handleSubmit(setData)} className="space-y-4">
+          <Input fieldname="Email" type="email" placeholder="Enter your email" {...register('email', { required: true })} />
+          <Button fieldname="Send" type="submit" />
+
+          {/* Opcional: Mensaje de error si el campo de correo electrónico es requerido */}
+          {/* {errors.email && <span>This field is required</span>} */}
+        </form>
+      </div>
     </div>
   );
 };
