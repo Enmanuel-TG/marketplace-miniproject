b87624b8c18ecaf904d33e0e67cea637a1f49912
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-08-15 22:27:44 -0400

    feat: update phone number validatation

--- backend/src/validators/auth.validator.ts
+++ backend/src/validators/auth.validator.ts
@@ -14,7 +14,11 @@ export const authRegisterValidator = z.object({
     .string({ required_error: 'Password is required.' })
     .min(4, { message: 'Password must to be least 4 characters.' }),
   birthday: z.string({ required_error: 'Birthday is required.' }),
-  phoneNumber: z.string({ required_error: 'Phone Number is required.' }),
+  phoneNumber: z
+    .string({ required_error: 'Phone Number is required.' })
+    .regex(/^\+?[0-9]+$/, { message: 'Invalid phone number. Only numbers are allowed without symbols or spaces.' })
+    .min(10, { message: 'Phone number is too short. Minimum 10 characters.' })
+    .max(15, { message: 'Phone number is too long. Maximum 15 characters.' }),
 });
 
 export const authLoginValidator = z.object({
