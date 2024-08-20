2591b43491dc7c10209d8065f2f285af85644d02
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-08-20 14:59:26 -0400

    add a menu

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -26,7 +26,6 @@ const SearchBar = () => {
             </svg>
           </div>
           <Input fieldname="" type="search" className="w-full pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search product" {...register('search', { required: true })} />
-          <button type="submit" className="text-white absolute right-2.5 bottom-1.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
         </div>
       </form>
       <select id="category" defaultValue="" onChange={handleCategoryChange} className="fl text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,24 +1,15 @@
 import { useProduct } from '../contexts/ProductContext';
 import { ProductCard } from '../components/ProductCard';
-import  IconProfile  from '../components/MyIconProfile';
 import SearchBar from '../components/SearchBar';
 import { toast } from 'react-toastify';
 import { toastifyConfig } from '../utilities/toastify.utility';
-import { useEffect } from 'react';
-import { useAuth } from '../contexts/AuthContext';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const HomePages = () => {
   const { allProducts, errors } = useProduct();
-  const { isAuthenticated } = useAuth();
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
-
-  const loginPage = () => {
-    navigate('/login');
-  };
-  const registerPage = () => {
-    navigate('/register');
-  };
 
   useEffect(() => {
     errors.map((error) => toast.error(error, toastifyConfig));
@@ -28,24 +19,24 @@ const HomePages = () => {
     <div className="flex flex-col">
       <div className='flex'>
         <SearchBar />
-        {isAuthenticated ? <IconProfile className='mt-1 mx-3 bg-white w-11 h-11 rounded-full object-cover no-select no-drag self-end' /> : <div className='flex h-10 mt-5'>
-          <button className="text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 px-3" onClick={registerPage} >Register</button>
-          <button className="text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mx-3 px-3" onClick={loginPage} >Login</button></div>}
       </div>
       <div className="max-w-screen w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {allProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      <a
-        href="#top"
-        className="fixed bottom-4 right-4 z-10 p-3 bg-blue-600 text-white rounded-full shadow-md transition-opacity duration-300"
-        aria-label="Scroll to top"
-      >
-      </a>
+      <div className='fixed bottom-10 right-4 z-10 py-11 text-white rounded-full shadow-md transition-opacity duration-300 '>
+        {menuOpen && <div className='flex flex-col'>
+          <img onClick={()=>{navigate('/create-product');}} className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400" src="/create.svg"/>
+          <img onClick={()=>{navigate('/profile');}} className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400" src="/profile.svg"/>
+          <img onClick={()=>{navigate('/profile');}} className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400" src="/myProduct.svg"/>
+          </div>}
+      <div onClick={() => setMenuOpen(!menuOpen)} className="fixed bottom-4 right-4 z-10 p-4 bg-blue-600 text-white rounded-full shadow-md transition-opacity duration-300 ">
+        <img src="/menu.svg" className='w-8'/>
+      </div>
+      </div>
     </div>
   );
 };
-
 export default HomePages;
 
