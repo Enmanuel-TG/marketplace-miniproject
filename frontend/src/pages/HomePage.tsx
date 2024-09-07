f98b01d6e1cdfea8740b7e9870dd41682575db1d
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-09-06 20:20:57 -0400

    fix: solve problem of error alert

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -23,12 +23,14 @@ const HomePages = () => {
   };
 
   useEffect(() => {
-    errors.map((error) => toast.error(error, toastifyConfig));
+    if (Array.isArray(errors)) {
+      errors.map((error) => toast.error(error, toastifyConfig));
+    }
   }, [errors]);
 
   return (
     <div className="flex flex-col">
-      <div className='flex'>
+      <div className="flex">
         <SearchBar />
       </div>
       <div className="max-w-screen w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
@@ -36,18 +38,40 @@ const HomePages = () => {
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      <div className='fixed bottom-10 right-4 z-10 py-11 text-white rounded-full shadow-md transition-opacity duration-300 '>
-        {menuOpen && <div className='flex flex-col'>
-          <img onClick={() => { verifyAuthStatus('create-product'); }} className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400" src="/create.svg" />
-          <img onClick={() => { verifyAuthStatus('profile'); }} className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400" src="/profile.svg" />
-          <img onClick={() => { verifyAuthStatus('profile'); }} className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400" src="/myProduct.svg" />
-        </div>}
-        <div onClick={() => setMenuOpen(!menuOpen)} className="fixed bottom-4 right-4 z-10 p-4 bg-blue-600 text-white rounded-full shadow-md transition-opacity duration-300 ">
-          <img src="/menu.svg" className='w-8' />
+      <div className="fixed bottom-10 right-4 z-10 py-11 text-white rounded-full shadow-md transition-opacity duration-300 ">
+        {menuOpen && (
+          <div className="flex flex-col">
+            <img
+              onClick={() => {
+                verifyAuthStatus('create-product');
+              }}
+              className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
+              src="/create.svg"
+            />
+            <img
+              onClick={() => {
+                verifyAuthStatus('profile');
+              }}
+              className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
+              src="/profile.svg"
+            />
+            <img
+              onClick={() => {
+                verifyAuthStatus('profile');
+              }}
+              className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
+              src="/myProduct.svg"
+            />
+          </div>
+        )}
+        <div
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="fixed bottom-4 right-4 z-10 p-4 bg-blue-600 text-white rounded-full shadow-md transition-opacity duration-300 "
+        >
+          <img src="/menu.svg" className="w-8" />
         </div>
       </div>
     </div>
   );
 };
 export default HomePages;
-
