4953f02d0a5b829f7368391909f59917ea6b307a
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-09-11 22:51:21 -0400

    feat: Add a Home menu component

---
+++ frontend/src/components/HomeMenu.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { toastifyConfig } from '../utilities/toastify.utility';
+import { toast } from 'react-toastify';
+
+export const HomeMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  const verifyAuthStatus = (where: string) => {
+    if (!isAuthenticated) {
+      toast.error('Please login first', toastifyConfig);
+      navigate('/login', { replace: true });
+      return;
+    }
+    navigate(`/${where}`);
+  };
+  return (
+    <div>
+      {menuOpen && (
+        <div className="flex flex-col">
+          <img
+            onClick={() => {
+              verifyAuthStatus('create-product');
+            }}
+            className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
+            src="/create.svg"
+          />
+          <img
+            onClick={() => {
+              verifyAuthStatus('profile');
+            }}
+            className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
+            src="/profile.svg"
+          />
+          <img
+            onClick={() => {
+              verifyAuthStatus('history');
+            }}
+            className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
+            src="/myProduct.svg"
+          />
+        </div>
+      )}
+      <div
+        onClick={() => setMenuOpen(!menuOpen)}
+        className="fixed bottom-4 right-4 z-10 p-4 bg-blue-600 text-white rounded-full shadow-md transition-opacity duration-300 "
+      >
+        <img src="/menu.svg" className="w-8" />
+      </div>
+    </div>
+  );
+};
+
+export default HomeMenu;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,26 +1,13 @@
+import { useEffect } from 'react';
 import { useProduct } from '../contexts/ProductContext';
 import { ProductCard } from '../components/ProductCard';
 import SearchBar from '../components/SearchBar';
+import HomeMenu from '../components/HomeMenu';
 import { toast } from 'react-toastify';
 import { toastifyConfig } from '../utilities/toastify.utility';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
 
 const HomePages = () => {
   const { allProducts, errors } = useProduct();
-  const { isAuthenticated } = useAuth();
-  const [menuOpen, setMenuOpen] = useState(false);
-  const navigate = useNavigate();
-
-  const verifyAuthStatus = (where: string) => {
-    if (!isAuthenticated) {
-      toast.error('Please login first', toastifyConfig);
-      navigate('/login', { replace: true });
-      return;
-    }
-    navigate(`/${where}`, { replace: true });
-  };
 
   useEffect(() => {
     if (Array.isArray(errors)) {
@@ -33,43 +20,14 @@ const HomePages = () => {
       <div className="flex">
         <SearchBar />
       </div>
-      <div className="max-w-screen w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      <div className="max-w-screen w-11/12 mx-auto grid grid-cols-1
+       sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {allProducts.map((product) => (
-          <ProductCard key={product.id} product={product} />
+          <ProductCard key={product.id} title="Buy" product={product} />
         ))}
       </div>
-      <div className="fixed bottom-10 right-4 z-10 py-11 text-white rounded-full shadow-md transition-opacity duration-300 ">
-        {menuOpen && (
-          <div className="flex flex-col">
-            <img
-              onClick={() => {
-                verifyAuthStatus('create-product');
-              }}
-              className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
-              src="/create.svg"
-            />
-            <img
-              onClick={() => {
-                verifyAuthStatus('profile');
-              }}
-              className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
-              src="/profile.svg"
-            />
-            <img
-              onClick={() => {
-                verifyAuthStatus('profile');
-              }}
-              className="bg-white my-2 p-2 placeholder: rounded-full shadow-md hover:bg-gray-400"
-              src="/myProduct.svg"
-            />
-          </div>
-        )}
-        <div
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="fixed bottom-4 right-4 z-10 p-4 bg-blue-600 text-white rounded-full shadow-md transition-opacity duration-300 "
-        >
-          <img src="/menu.svg" className="w-8" />
-        </div>
+      <div className="fixed bottom-10 right-4 z-10 py-11 text-white rounded-full shadow-md        transition-opacity duration-300 ">
+        <HomeMenu />
       </div>
     </div>
   );
