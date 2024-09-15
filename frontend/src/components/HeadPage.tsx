df24a1152a833a121d295642ea05febc335b74a6
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-09-14 23:43:08 -0400

    feat: add a headPage component and create a button to back to home Page

---
+++ frontend/src/components/HeadPage.tsx
@@ -0,0 +1,16 @@
+import { ButtonBack } from './ui/ButtonBack';
+import MyIconProfile from './ui/MyIconProfile';
+
+const HeadPage = ({namePage}: {namePage: string}) => {
+  return (
+    <div>
+      <header className="flex px-4">
+        <ButtonBack styles="mt-4" />
+        <h1 className="text-3xl font-bold mb-8 text-white ml-4 mt-4 no-drag no-select">{namePage}</h1>
+        <MyIconProfile className="mx-3 bg-white w-11 h-11 rounded-full no-select no-drag absolute right-5 top-5 shadow-lg" />
+      </header>
+    </div>
+  );
+};
+
+export default HeadPage;
--- frontend/src/pages/HistoryPage.tsx
+++ frontend/src/pages/HistoryPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useProduct } from '../contexts/ProductContext';
 import { ProductCard } from '../components/ProductCard';
+import HeadPage from '../components/HeadPage';
 
 const HistoryPage = () => {
   const { getAllUSerProducts, allProducts } = useProduct();
@@ -9,10 +10,13 @@ const HistoryPage = () => {
   }, []);
 
   return (
-    <div className="max-w-screen w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 pt-12">
-      {allProducts.map((product) => (
-        <ProductCard title='See Details' key={product.id} product={product} />
-      ))}
+    <div>
+      <HeadPage namePage="History" />
+      <div className="max-w-screen w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 ">
+        {allProducts.map((product) => (
+          <ProductCard title="See Details" key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
