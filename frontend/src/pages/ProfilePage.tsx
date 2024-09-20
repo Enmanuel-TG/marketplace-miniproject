a5fc6b527b7410ad10b9844cca30d83000fbfc49
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-09-19 22:38:20 -0400

    style: Add styles to make the profile page responsive

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -15,21 +15,24 @@ const ProfilePage = () => {
 
   return (
     <Dialog>
-      <div className="bg-gray-900 mt-10 flex w-2/3 mx-auto h-screen no-drag no-select">
-        <div className="flex items-center ml-20 p-10 gap-8">
-          <div>
-            <img src={user?.photo} className="w-48 h-48 no-select no-drag rounded-full shadow-lg" />
+      <div className="bg-gray-900 mt-10 mx-auto w-full max-w-[80vw] no-drag no-select p-[4vw] sm:p-[3vw] lg:p-[2vw] rounded-lg shadow-md">
+        <div className="flex flex-col lg:flex-row gap-[5vw] items-center lg:items-start">
+          <div className="relative">
+            <img
+              src={user?.photo}
+              className="no-select no-drag rounded-full shadow-lg w-[25vw] h-[25vw] sm:w-[20vw] sm:h-[20vw] lg:w-[15vw] lg:h-[15vw] object-cover"
+            />
             <DialogTrigger
-              className="relative bottom-7 left-36 px-3 py-1 no-select no-drag bg-blue-500 text-white rounded-lg"
+              className="absolute bottom-[1vw] right-[1vw] px-[1vw] py-[0.5vw] bg-blue-500 text-white rounded-lg"
               onClick={() => setIsEdit(true)}
             >
               Edit
             </DialogTrigger>
           </div>
-          <div>
-            <p className="text-white text-2xl font-semibold">{user?.name}</p>
+          <div className="text-center lg:text-left">
+            <p className="text-white mt-7 text-[3vw] sm:text-[3vw] lg:text-[2.5vw] font-semibold">{user?.name}</p>
             {user?.createdAt && (
-              <div className="text-white">
+              <div className="text-white mt-[1vw] text-[2.5vw] sm:text-[1.5vw] lg:text-[1vw]">
                 Joined on{' '}
                 {new Date(user?.createdAt).toLocaleDateString('en-US', {
                   month: 'long',
@@ -40,12 +43,17 @@ const ProfilePage = () => {
           </div>
         </div>
       </div>
-      <DialogContent>
+      <DialogContent className="p-[3vw]">
         <DialogHeader>
-          <DialogTitle>Change your photo profile</DialogTitle>
+          <DialogTitle className="text-[4vw] lg:text-[2vw]">Change your profile photo</DialogTitle>
         </DialogHeader>
-        <img src={previewPhoto || user?.photo} className="w-48 h-48 no-select no-drag rounded-full shadow-lg" />
-        <GetPicture onPhotoChange={setPreviewPhoto} />
+        <div className="flex flex-col items-center">
+          <img
+            src={previewPhoto || user?.photo}
+            className="w-[25vw] h-[25vw] sm:w-[20vw] sm:h-[20vw] lg:w-[15vw] lg:h-[15vw] no-select no-drag rounded-full shadow-lg object-cover"
+          />
+          <GetPicture onPhotoChange={setPreviewPhoto} />
+        </div>
       </DialogContent>
     </Dialog>
   );
