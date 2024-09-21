190b716903a2085f819022f94117cc104d8cd5a4
Enmanuel-TG <enmanueltaveras06@gmail.com>
2024-09-21 17:59:10 -0400

    feat: add endpoint to update user data

--- backend/src/router/user.router.ts
+++ backend/src/router/user.router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { profile, updatePhotoProfile, changePassword, getUser } from '../controllers/user.controller';
+import { profile, updatePhotoProfile, changePassword, getUser, updateUser } from '../controllers/user.controller';
 import validateToken from '../middlewares/validate-token.middleware.ts';
 import validateSchema from '../middlewares/validator.middleware.ts';
 import { passwordValidator } from '../validators/user.validator.ts';
+import { UpdateProfileValidator } from '../validators/update-profile.validator.ts';
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.get('/profile', validateToken, profile);
 router.post('/update', validateToken, updatePhotoProfile);
 router.get('/get-user', getUser);
 router.put('/change-password', validateSchema(passwordValidator), changePassword);
+router.put('/profile', validateToken, validateSchema(UpdateProfileValidator), updateUser);
 
 export default router;
---
+++ backend/src/validators/update-profile.validator.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const UpdateProfileValidator = z.object({
+  name: z.string({ required_error: 'Name is required.' }).refine(
+    (value) => {
+      return value.trim().length > 0;
+    },
+    {
+      message: 'String must not be empty or contain only spaces',
+    },
+  ),
+  birthday: z.string({ required_error: 'Birthday is required.' }),
+  phoneNumber: z
+    .string({ required_error: 'Phone Number is required.' })
+    .regex(/^\+?[0-9]+$/, { message: 'Invalid phone number. Only numbers are allowed without symbols or spaces.' })
+    .min(10, { message: 'Phone number is too short. Minimum 10 characters.' })
+    .max(15, { message: 'Phone number is too long. Maximum 15 characters.' }),
+});
